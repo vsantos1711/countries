fa223cdf09a36a7e4a0413208fb6efee1f03c1de
vsantos1711 <vsantos067100@gmail.com>
2023-12-17 19:04:55 -0300

    feat: create not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-red-500 text-4xl font-semibold ">
+        404 Pag not found
+      </h1>
+    </div>
+  );
+}
