3e4a6c9cc2d2f663701d177f85a0d5a1997a1480
vsantos1711 <vsantos067100@gmail.com>
2023-12-17 19:00:47 -0300

    style: add shadow on header component

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Head() {
   return (
-    <nav className="flex justify-around bg-white p-5 ">
-      <button>Where in the world?</button>
+    <nav className="flex justify-around bg-white p-3 shadow ">
+      <Link href={"/"}>
+        <button>Where in the world?</button>
+      </Link>
     </nav>
   );
 }
