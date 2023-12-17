a13e6302b504dd73bb1548609d96b6a2ea186874
vsantos1711 <vsantos067100@gmail.com>
2023-12-17 17:02:35 -0300

    Initial commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Countries app</h1>;
+}
---
+++ src/app/head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <nav className="flex justify-around bg-white p-5 ">
+      <button>Where in the world?</button>
+    </nav>
+  );
+}
