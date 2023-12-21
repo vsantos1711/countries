c28c2d46cc0634cf1bb254c3ef382952dd0d8f2d
vsantos1711 <vsantos067100@gmail.com>
2023-12-20 22:58:04 -0300

    feat: add countries request and Card component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import Card from "@/components/Card/Card";
+
+async function getCountries() {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  return res.json();
+}
+export default async function Home() {
+  const countries = await getCountries();
+
   return (
-    <div className="min-h-screen dark:bg-gray-800 ">
-      <h1>Countries app</h1>
-    </div>
+    <main className="min-h-screen dark:bg-gray-800 mx-auto ">
+      {countries.map((country: any) => (
+        <Card
+          key={country.name.common}
+          img={country.flags.svg}
+          name={country.name.common}
+          population={country.population}
+          region={country.region}
+          capital={country.capital}
+          link={`/country/${country.name.common}`}
+        />
+      ))}
+    </main>
   );
 }
