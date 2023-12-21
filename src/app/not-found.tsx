ffb331b9ab6c56f9d386efb931999591626525ba
vsantos1711 <vsantos067100@gmail.com>
2023-12-20 22:57:32 -0300

    style: add dark mode at not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center dark:bg-gray-800">
       <h1 className="text-red-500 text-4xl font-semibold ">
         404 Pag not found
       </h1>
